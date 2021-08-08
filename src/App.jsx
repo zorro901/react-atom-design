68bf8caa9cc6fb85b458908d79f7166c6d0ba2a2
zorro901 <zoro907@gmail.com>
2021-08-08 21:13:03 +0900

    29. Organismの作成1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
       <SearchInput />
+      <UserCard />
     </div>
   );
 }
---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img src="" alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>aaa@ac.com</dd>
+        <dt>TEL</dt>
+        <dd>000-0000-1111</dd>
+        <dt>会社名</dt>
+        <dd>株式会社</dd>
+        <dt>WEB</dt>
+        <dd>tttt.com</dd>
+      </dl>
+    </div>
+  );
+};
