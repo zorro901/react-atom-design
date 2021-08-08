b89a46838a19434fb699fb1bffd7ae7e330936da
zorro901 <zoro907@gmail.com>
2021-08-08 20:44:29 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
+    </div>
+  );
+}
