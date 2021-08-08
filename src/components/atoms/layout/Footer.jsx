435c4bf9863ae37ac987c20ccbbbc8ac1e38c3aa
zorro901 <zoro907@gmail.com>
2021-08-09 00:18:14 +0900

    32. Pageの作成

--- src/components/atoms/layout/Footer.jsx
+++ src/components/atoms/layout/Footer.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+
 export const Footer = () => {
   return <SFooter>&copy; 2021 test Inc.</SFooter>;
 };
 
-const SFooter = styled.header`
+const SFooter = styled.footer`
   background-color: #11999e;
   color: #fff;
   text-align: center;
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `test${val}`,
+    image: "https://source.unsplash.com/_PzZxrEswRY",
+    email: "12345@excamol.com",
+    phone: "000-1111-2222",
+    company: {
+      name: "test株式会社"
+    },
+    website: "http://googme.com"
+  };
+});
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
